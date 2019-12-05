caebb0582372fffe8adf2047e742bf9534aaadec
cdeath <github@nunopereira.net>
2019-12-05 10:55:00 +0000

    all methods

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 (function() {
 
+  /* UTILS */
   function qs(sel, ctx) {
     ctx = ctx || document;
     return ctx.querySelector(sel);
@@ -9,63 +10,126 @@
     return [].slice.call(obj);
   }
 
-  var list = [],
+  function findIndex(elm, parent) {
+    var elms = toArray(parent.children);
+    return elms.indexOf(elm);
+  }
+
+  function getValue(elm) {
+    return elm.type === 'checkbox' ? elm.checked : elm.value;
+  }
+
+  function loadData(key) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  function saveData(key, data) {
+    try {
+      localStorage.setItem(key, JSON.stringify(data));
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  /* VARS */
+  var list = loadData('shoppingList') || [],
       ul = qs('#shopping-list'),
       form = qs('#shopping-list-form'),
-      emptyMessage = qs('#empty-message');
+      emptyMessage = qs('#empty-message'),
+      moodText = qs('.mood-text'),
+      moodEmoji = qs('.mood-emoji');
 
-  function renderItem(name, quantity, done) {
+  /* METHODS */
+  function renderItem(item) {
     return [
       '<li class="list-group-item d-flex align-items-center gutters">',
-        '<input type="checkbox" aria-label="done"' + (done ? ' checked' : '') + '>',
-        '<input type="text" class="form-control" placeholder="Name" value="' + name + '">',
-        '<input type="text" class="form-control w-25" placeholder="Quantity" value="' + quantity + '">',
-        '<button type="button" class="btn btn-outline-danger"><svg><use xlink:href="#icon-remove"></use></svg></button>',
+        '<input type="checkbox" name="done" aria-label="done"' + (item.done ? ' checked' : '') + '>',
+        '<input type="text" name="name" class="form-control" placeholder="Name" value="' + item.name + '">',
+        '<input type="text" name="quantity" class="form-control w-25" placeholder="Quantity" value="' + item.quantity + '">',
+        '<button type="button" class="btn btn-outline-danger" data-action="remove"><svg><use xlink:href="#icon-remove"></use></svg></button>',
       '</li>'
     ].join('');
   }
 
-  function saveList() {
-
-  }
-
-  function checkListStatus() {
+  function updateListStatus() {
     emptyMessage.classList.toggle('d-none', list.length);
+    moodText.innerHTML = list.length ? 'Awesome' : 'Sad';
+    moodEmoji.innerHTML = list.length ? '😎' : '😢';
   }
 
-  function addItem(name, quantity, done) {
-    list.push({name: name, quantity: quantity, done: done});
-    ul.innerHTML += renderItem(name, quantity, done);
-    checkListStatus();
-    saveList();
+  function addItem(item) {
+    list.push(item);
+    ul.innerHTML += renderItem(item);
+    updateListStatus();
+    saveData('shoppingList', list);
   }
 
-  function removeItem(index) {
-
+  function removeItem(elm) {
+    var li = elm.parentNode,
+        index = findIndex(li, ul);
+    
+    list.splice(index, 1);
+    ul.removeChild(li);
+    updateListStatus();
+    saveData('shoppingList', list);
   }
 
-  function editItem(index) {
+  function updateItem(elm) {
+    var li = elm.parentNode,
+        index = findIndex(li, ul);
 
+    list[index][elm.name] = getValue(elm);
+    saveData('shoppingList', list);
   }
 
-  form.addEventListener('submit', function(evt) {
+  /* EVENT HANDLERS */
+  function handleFormSubmit(evt) {
     evt.preventDefault();
-    var elements = toArray(evt.target.elements);
-    var obj = {};
+    var formElements = toArray(evt.target.elements),
+        item = {
+          done: false,
+        };
 
-    elements.forEach(function(element) {
-      obj[element.name] = element.type === 'checkbox' ? element.checked : element.value;
+    formElements.forEach(function(elm) {
+      if (elm.type !== 'submit') {
+        item[elm.name] = getValue(elm);
+      }
     });
 
-    addItem(obj.name, obj.quantity, obj.done);
-
+    addItem(item);
     form.reset();
-  });
+  }
 
-  checkListStatus();
+  function handleListEvents(evt) {
+    var elm = evt.target;
+    switch (true) {
+      case evt.type === 'click' && elm.matches('[data-action="remove"]'):
+        removeItem(elm);
+        break;
+      default:
+        updateItem(elm);
+        break;
+    }
+  }
 
+  /* EVENT LISTENERS */
+  form.addEventListener('submit', handleFormSubmit);
+  ul.addEventListener('click', handleListEvents);
+  ul.addEventListener('input', handleListEvents);
+
+  /* INIT */
+  updateListStatus();
+  ul.innerHTML = list.map(function(item) {
+    return renderItem(item);
+  }).join('');
+
+  /* EXPORT (optional) */
   window.awesomeShoppingList = {
-    addItem: addItem
+    addItem: addItem,
   };
 
 })();
